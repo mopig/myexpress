d80132802dff0f839b40a290f160eea84902e3bb
mopig <mopig@foxmail.com>
2014-04-18 16:29:36 +0800

    finish

--- test/app_spec.js
+++ test/app_spec.js
@@ -1,7 +1,28 @@
 var express = require("../");
 var request = require("supertest");
+var expect = require("chai").expect;
+var http = require("http");
 
 describe("app", function () {
-    describe("create http server"), function () {
-        it('')
-      }})
+    var app = express();
+    describe("create http server", function () {
+        it('create http server', function(done) {
+            var server = http.createServer(app);
+            request(server).get('/foo').expect(404).end(done);
+        });
+      });
+
+    describe("define a method named listen for app", function() {
+        var port = 4000;
+        var server;
+        before(function(done) {
+            server = app.listen(port, done);
+        })
+        it("invoke listen", function() {
+            expect(server).to.be.instanceof(http.Server);
+        });
+        it("it will responds 404", function(done) {
+            request("http://localhost:" + port).get("/foo").expect(404).end(done);
+        })
+    });
+  });
